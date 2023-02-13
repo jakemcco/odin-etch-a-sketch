0d1bed9042aa0d17796b65c7848fcf356a67ca97
Jake McCormick <122489471+jakemcco@users.noreply.github.com>
2023-02-13 11:59:37 -0500

    Add grid size slider and clear grid inputs

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 
 
-const gridSize = 16;
+const gridSizeSlider = document.getElementById('gridSizeSlider');
+const gridSizeValue = document.getElementById('gridSizeValue');
+const drawColor = 'white';
 const gridContainer = document.getElementById('grid-container');
+
+//Button variables
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.onclick = ()=> resetGrid();
+
+let gridSize = gridSizeSlider.value;
+gridSizeValue.textContent = `${gridSize} x ${gridSize}`;
+
+gridSizeSlider.onchange = () =>{
+    gridSize = gridSizeSlider.value;
+    gridSizeValue.textContent = `${gridSize} x ${gridSize}`;
+    resetGrid();
+}
+//Monitor mouse click status for whether user wants to draw or just hover
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown) = true
+document.body.onmouseup = () => (mouseDown) = false
 
 function initializeGrid(size){
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -10,20 +30,23 @@ function initializeGrid(size){
     for (let i =0; i < (size ** 2); i++){
         const gridItem = document.createElement('div')
         gridItem.classList.add('grid-item');
-        gridItem.textContent = i
-        gridItem.addEventListener('mouseenter', (e) =>{
-            changeColor(e.target,'white');
-        })
-        gridItem.addEventListener('mouseleave', (e) =>{
-            changeColor(e.target,'');
-        })
-
+        gridItem.addEventListener('mouseover', changeColor)
+        gridItem.addEventListener('mousedown', changeColor)
         gridContainer.appendChild(gridItem)
     }
 }
 
-function changeColor(item, color){
-    item.style.backgroundColor = color;
+function changeColor(e){
+    console.log(e)
+    if (e.type === 'mousedown' || (mouseDown && e.type === 'mouseover')){
+        e.target.style.backgroundColor = drawColor
+    }
+    else return;
+}
+
+function resetGrid(){
+    gridContainer.replaceChildren(); //https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    initializeGrid(gridSize);
 }
 
 initializeGrid(gridSize);
